87a4d3121ad11fe574d469657dd258798c5ba428
maxkurilskii <dogger.bob@mail.ru>
2024-02-28 20:16:20 +0300

    Made my first package

---
+++ scripts/first_node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+current_time = datetime.now().strftime("%H:%M:%S")
+next_time = current_time + 5
+print("Текущее время:", current_time)
+while(True):
+ current_time = datetime.now().strftime("%H:%M:%S")
+ if current_time == next_time:
+   print("Текущее время:", current_time)
+   next_time = current_time+5
+
